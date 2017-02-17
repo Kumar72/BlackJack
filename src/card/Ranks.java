1571e829458c2c3ca9620fb9e5a084e22fff69d1
Kumar72 <vichandan@gmail.com>
2017-02-17 16:04:27 -0700

    The ENUM files

---
+++ src/card/Ranks.java
@@ -0,0 +1,5 @@
+package card;
+
+public enum Ranks {
+	TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE
+}
---
+++ src/card/Suits.java
@@ -0,0 +1,5 @@
+package card;
+
+public enum Suits {
+	SPADES,HEARTS,DIAMONDS,CLUBS
+}
